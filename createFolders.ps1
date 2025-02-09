99fb0d73f91f31baec0993dbc2ef392e222b49dd
Steven Clerc <clerc.steven@gmail.com>
2025-02-09 16:05:57 +0100

    Update createFolders.ps1

--- createFolders.ps1
+++ createFolders.ps1
@@ -1,20 +1,57 @@
-[reflection.assembly]::LoadWithPartialName("System.Drawing");
-$dates = [System.Collections.ArrayList]::new()
+[reflection.assembly]::LoadWithPartialName("System.Drawing")
 
-$test = Get-ChildItem -path ./* -Include *.jpg | ForEach-Object {
-  $imagePath = "./" + $_.Name
-  $pic= New-Object System.Drawing.Bitmap $imagePath
+# 36867 date Taken (PropertyTagExifDTOrig)
+# 36868 PropertyTagExifDTDigitized
+# 306 creation date (PropertyTagDateTime)
+# 800 file title (PropertyTagImageTitle)
+$metadataCodes = @(36867, 36868, 306, 800);
 
-  $picDate = $pic.GetPropertyItem(36867).value[0..9]
+# hastable where key = folderName; value = fileName
+$sortFiles = [ordered]@{};
 
-  $strYear = [String][Char]$picdate[0]+[String][Char]$picdate[1]+[String][Char]$picdate[2]+[String][Char]$picdate[3]
-  $strMonth = [String][Char]$picdate[5]+[String][Char]$picdate[6]
-  $strDay = [String][Char]$picdate[8]+[String][Char]$picdate[9]
-  $dates.Add($strYear + "-" + $strMonth)
+function parseDate {
+  param (
+    $picDate
+  )
+
+    $strYear = [String][Char]$picDate[0]+[String][Char]$picDate[1]+[String][Char]$picDate[2]+[String][Char]$picDate[3]
+    $strMonth = [String][Char]$picDate[5]+[String][Char]$picDate[6]
+    $strDay = [String][Char]$picDate[8]+[String][Char]$picDate[9]
+    return $strYear + "-" + $strMonth;
 }
-$dates = $dates | select -Unique
-$dates
 
-foreach ($date in $dates) {
-  New-Item -Path . -Name ($date) -ItemType Directory
+$test = Get-ChildItem -path ./* -Include ('*.jpg', '*.png', '*.jpeg' ) | ForEach-Object {
+  $folderName;
+  $fileInformations = Get-ItemProperty -Path $_ ;
+  $pic = [System.Drawing.Bitmap]::FromFile(($_.FullName));
+
+  if ([bool]$pic.PropertyIdList.Contains($metadataCodes[0])) {
+    $picDate = $pic.GetPropertyItem($metadataCodes[0]).value[0..9];
+    $folderName = parseDate($picDate);
+  } ElseIf ([bool]$pic.PropertyIdList.Contains($metadataCodes[1])) {
+    $picDate = $pic.GetPropertyItem($metadataCodes[1]).value[0..9];
+    $folderName = parseDate($picDate);
+  } ElseIf ([bool]$pic.PropertyIdList.Contains($metadataCodes[2])) {
+    $picDate = $pic.GetPropertyItem($metadataCodes[2]).value[0..9];
+    $folderName = parseDate($picDate);
+  } ElseIf ([bool]$_.LastWriteTime) {
+    $date = $_.LastWriteTime | Get-Date -Format "yyyy-MM-dd"
+    $folderName = parseDate($date);
+  } else {
+    Write-Host "no date found for file: " + $_.Name;
+  }
+
+  if ([bool]$sortFiles.Contains($folderName)) {
+    $sortFiles[$folderName] += $_;
+  } else {
+    $sortFiles.Add($folderName, @($_));
+  }
+}
+
+foreach ($folder in $sortFiles.Keys) {
+  $Destionation = New-Item -Path . -Name ($folder) -ItemType Directory -Force
+
+  foreach ($file in $sortFiles[$folder]) {
+    Copy-Item -Path ($file.FullName) -Destination $Destionation.FullName
+  }
 }
